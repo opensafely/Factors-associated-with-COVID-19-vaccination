1eaac05ceb9021cec27c2131a597c5caec4da7fc
acagreen17 <acagreen17@gmail.com>
2021-04-09 19:50:53 +0100

    Update scripts

---
+++ analysis/R/Scripts/00_process_data.R
@@ -0,0 +1,271 @@
+######################################
+
+# This script:
+# - imports data extracted by the cohort extractor
+# - combines ethnicity columns
+# - calculates survival time
+# - standardises some variables (eg convert to factor) and derives some new ones
+# - saves processed one-row-per-patient dataset
+
+######################################
+
+
+# Preliminaries ----
+
+## Import libraries
+library('tidyverse')
+library('lubridate')
+
+## Custom functions
+### tte
+tte <- function(origin_date, event_date, censor_date, na.censor=FALSE){
+  # returns time-to-event date or time to censor date, which is earlier
+  
+  if (na.censor)
+    time <- event_date-origin_date
+  else
+    time <- pmin(event_date-origin_date, censor_date-origin_date, na.rm=TRUE)
+  as.numeric(time)
+}
+
+### Factorise
+fct_case_when <- function(...) {
+  # uses dplyr::case_when but converts the output to a factor,
+  # with factors ordered as they appear in the case_when's  ... argument
+  args <- as.list(match.call())
+  levels <- sapply(args[-1], function(f) f[[3]])  # extract RHS of formula
+  levels <- levels[!is.na(levels)]
+  factor(dplyr::case_when(...), levels=levels)
+}
+
+## Output processed data to rds
+dir.create(here::here("output", "data"), showWarnings = FALSE, recursive=TRUE)
+
+
+# Process data ----
+
+## Print variable names
+read_csv(here::here("output", "input.csv"),
+         n_max = 0,
+         col_types = cols()) %>%
+  names() %>%
+  print()
+
+## Read in data (don't rely on defaults)
+data_extract0 <- read_csv(
+  here::here("output", "input.csv"),
+  col_types = cols_only(
+    
+    # Identifier
+    patient_id = col_integer(),
+    
+    # Outcome
+    covid_vax_1_date = col_date(format="%Y-%m-%d"),
+    
+    # Censoring
+    death_date = col_date(format="%Y-%m-%d"),
+    dereg_date = col_date(format="%Y-%m-%d"),
+    
+    # Demographic
+    age = col_integer(),
+    sex = col_character(),
+    ethnicity = col_character(),
+    ethnicity_other = col_date(format="%Y-%m-%d"),
+    ethnicity_not_given = col_date(format="%Y-%m-%d"),
+    ethnicity_not_stated = col_date(format="%Y-%m-%d"),
+    ethnicity_no_record = col_date(format="%Y-%m-%d"),
+    
+    # Clinical measurements and comorbidities
+    bmi = col_double(),
+    bmi_stage_date = col_date(format="%Y-%m-%d"),
+    sev_obesity = col_date(format="%Y-%m-%d"),
+    chronic_heart_disease = col_logical(),
+    diabetes = col_logical(),
+    chronic_kidney_disease_diagnostic = col_logical(),
+    chronic_kidney_disease_all_stages = col_logical(),
+    chronic_kidney_disease_all_stages_1_5 = col_logical(),
+    sev_mental_ill = col_date(format="%Y-%m-%d"),
+    learning_disability = col_date(format="%Y-%m-%d"),
+    chronic_neuro_dis_inc_sig_learn_dis = col_date(format="%Y-%m-%d"),
+    stroke = col_logical(),
+    asplenia = col_logical(),
+    chronic_liver_disease = col_logical(),
+    chronis_respiratory_disease = col_date(format="%Y-%m-%d"),
+    immunosuppression_diagnosis = col_date(format="%Y-%m-%d"),
+    immunosuppression_medication = col_date(format="%Y-%m-%d"),
+    
+    # Geographical
+    practice_id = col_integer(),
+    imd = col_character(),
+    region = col_character(),
+    stp = col_character(),
+    rural_urban = col_character(),
+    
+    # Other
+    flu_vaccine = col_logical(),
+    shielded = col_logical(),
+    shielded_since_feb_15 = col_logical(),
+    hhld_imdef_dat = col_date(format="%Y-%m-%d")
+  ),
+  na = character() # more stable to convert to missing later
+)
+
+## Parse NAs
+data_extract <- data_extract0 %>%
+  mutate(across(
+    .cols = where(is.character),
+    .fns = ~na_if(.x, "")
+  )) %>%
+  mutate(across(
+    .cols = c(where(is.numeric), -ends_with("_id")), #convert numeric+integer but not id variables
+    .fns = ~na_if(.x, 0)
+  )) %>%
+  arrange(patient_id) %>%
+  select(all_of((names(data_extract0))))
+
+
+
+## Format columns (i.e, set factor levels)
+data_processed <- data_extract %>%
+  mutate(
+    
+    # COVID vaccination
+    covid_vax = as.integer(ifelse(is.na(covid_vax_1_date), 0, 1)),
+    
+    # Censoring
+    censor_date = pmin(death_date, 
+                       dereg_date, 
+                       as.Date("2021-04-01", format = "%Y-%m-%d"), 
+                       na.rm=TRUE),
+    
+    # Follow-up time
+    follow_up_time = tte(as.Date("2020-12-07", format = "%Y-%m-%d"),
+                         covid_vax_1_date,
+                         censor_date),
+    
+    # Age
+    ageband = cut(
+      age,
+      breaks = c(-Inf, 85, 90, 95, Inf),
+      labels = c("80-84", "85-89", "90-94", "95+"),
+      right = FALSE
+    ),
+    
+    # Sex
+    sex = fct_case_when(
+      sex == "F" ~ "Female",
+      sex == "M" ~ "Male",
+      #sex == "I" ~ "Inter-sex",
+      #sex == "U" ~ "Unknown",
+      TRUE ~ NA_character_
+    ),
+    
+    # Ethnicity
+    ethnicity =  ifelse(is.na(ethnicity) & !is.na(ethnicity_other), 17, 
+                        ifelse(is.na(ethnicity) & !is.na(ethnicity_not_given), 18,
+                               ifelse(is.na(ethnicity) & !is.na(ethnicity_not_stated), 19,
+                                      ifelse(is.na(ethnicity) & !is.na(ethnicity_no_record), 20,
+                                             ethnicity)))),
+    
+    ethnicity = ifelse(is.na(ethnicity), 20, ethnicity),
+    
+    ethnicity = fct_case_when(
+      ethnicity == "1" ~ "White - British",
+      ethnicity == "2" ~ "White - Irish",
+      ethnicity == "3" ~ "White - Any other White background",
+      ethnicity == "4" ~ "Mixed - White and Black Caribbean",
+      ethnicity == "5" ~ "Mixed - White and Black African",
+      ethnicity == "6" ~ "Mixed - White and Asian",
+      ethnicity == "7" ~ "Mixed - Any other mixed background",
+      ethnicity == "8" ~ "Asian or Asian British - Indian",
+      ethnicity == "9" ~ "Asian or Asian British - Pakistani",
+      ethnicity == "10" ~ "Asian or Asian British - Bangladeshi",
+      ethnicity == "11" ~ "Asian or Asian British - Any other Asian background",
+      ethnicity == "12" ~ "Black or Black British - Caribbean",
+      ethnicity == "13" ~ "Black or Black British - African",
+      ethnicity == "14" ~ "Black or Black British - Any other Black background",
+      ethnicity == "15" ~ "Other ethnic groups - Chinese",
+      ethnicity == "16" ~ "Other ethnic groups - Any other ethnic group",
+      ethnicity == "17" ~ "Patients with any other ethnicity code",
+      ethnicity == "18" ~ "Ethnicity not given - patient refused",
+      ethnicity == "19" ~ "Ethnicity not stated",
+      ethnicity == "20" ~ "Ethnicity not recorded",
+      #TRUE ~ "Unknown",
+      TRUE ~ NA_character_
+    ),
+    
+    # Morbidly obese
+    morbid_obesity_bmi = ifelse(bmi >= 40, 1, 0),
+    morbid_obesity_date = ifelse(sev_obesity >= bmi_stage_date, 1, 0),
+    morbid_obesity = ifelse(morbid_obesity_date == 1 & morbid_obesity_bmi == 0, 1, morbid_obesity_bmi),
+    morbid_obesity = ifelse(is.na(morbid_obesity), 0, morbid_obesity),
+    
+    # Mental illness
+    sev_mental_ill = ifelse(is.na(sev_mental_ill), FALSE, TRUE),
+    
+    # Learning disability
+    learning_disability = ifelse(is.na(learning_disability), FALSE, TRUE),
+    
+    # CND inc LD
+    chronic_neuro_dis_inc_sig_learn_dis = ifelse(is.na(chronic_neuro_dis_inc_sig_learn_dis), FALSE, TRUE),
+    
+    #CRD
+    chronis_respiratory_disease = ifelse(is.na(chronis_respiratory_disease), FALSE, TRUE),
+    
+    # Immunosuppression
+    immunosuppression_diagnosis = ifelse(is.na(immunosuppression_diagnosis), FALSE, TRUE),
+    immunosuppression_medication = ifelse(is.na(immunosuppression_medication), FALSE, TRUE),
+    
+    # IMD
+    imd = na_if(imd, "0"),
+    imd = fct_case_when(
+      imd == 1 ~ "1 most deprived",
+      imd == 2 ~ "2",
+      imd == 3 ~ "3",
+      imd == 4 ~ "4",
+      imd == 5 ~ "5 least deprived",
+      #TRUE ~ "Unknown",
+      TRUE ~ NA_character_
+    ),
+    
+    # Region
+    region = factor(region,
+                    levels = c(
+                      "East",
+                      "East Midlands",
+                      "London",
+                      "North East",
+                      "North West",
+                      "South East",
+                      "South West",
+                      "West Midlands",
+                      "Yorkshire and The Humber"
+                    )
+    ),
+    
+    # stp
+    stp = as.factor(stp),
+    
+    # Rural/urban
+    rural_urban = as.factor(rural_urban),
+    
+    # Household contact of shielding individual
+    hhld_imdef_dat = ifelse(is.na(hhld_imdef_dat), FALSE, TRUE)
+    
+  ) %>%
+  select(patient_id, covid_vax, follow_up_time, practice_id, stp, age, ageband, sex, ethnicity, morbid_obesity, chronic_heart_disease,
+         diabetes, chronic_kidney_disease_diagnostic, chronic_kidney_disease_all_stages, chronic_kidney_disease_all_stages_1_5,
+         sev_mental_ill, learning_disability, chronic_neuro_dis_inc_sig_learn_dis, stroke, asplenia, chronic_liver_disease, 
+         chronis_respiratory_disease, immunosuppression_diagnosis, immunosuppression_medication, imd, region, rural_urban, 
+         flu_vaccine, shielded, shielded_since_feb_15) %>%
+  filter(age >= 80,
+         sex %in% c("Male", "Female"),
+         !is.na(imd),
+         !is.na(ethnicity),
+         !is.na(region),
+         !is.na(rural_urban)
+         )
+
+
+# Save dataset as .rds files ----
+write_rds(data_processed, here::here("output", "data", "data_all.rds"), compress="gz")
