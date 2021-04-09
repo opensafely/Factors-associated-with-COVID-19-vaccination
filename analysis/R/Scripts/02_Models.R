4bed28506ef9db1608dc3cca2e34453a4d9f315f
acagreen17 <acagreen17@gmail.com>
2021-04-09 16:42:49 +0100

    Remove mixed models to test on job server

--- analysis/R/Scripts/02_Models.R
+++ analysis/R/Scripts/02_Models.R
@@ -14,9 +14,10 @@
 library('tidyverse')
 library('lubridate')
 library('survival')
-library('coxme')
+#library('coxme')
 library('gtsummary')
 library('gt')
+#library('ehahelper')
 
 ## Create output directories
 dir.create(here::here("output", "models"), showWarnings = FALSE, recursive=TRUE)
@@ -54,16 +55,16 @@ mod.coxph.adj <- coxph(Surv(follow_up_time, covid_vax) ~
 write_rds(mod.coxph.unadj, here::here("output", "models", "mod_coxph_adj.rds"), compress="gz")
 
 ## Mixed effects Cox model - adjusted; baseline demographics, comorbs, geographical, other, stp as random effect
-mod.coxme.adj <- coxme(Surv(follow_up_time, covid_vax) ~ 
-                          age + sex + ethnicity + morbid_obesity +
-                          chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
-                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
-                          stroke + asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
-                          immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
-                          shielded_since_feb_15 + (1 | practice_id),
-                        data = data_cox)
-
-write_rds(mod.coxme.adj, here::here("output", "models", "mod_coxme_adj.rds"), compress="gz")
+# mod.coxme.adj <- coxme(Surv(follow_up_time, covid_vax) ~ 
+#                           age + sex + ethnicity + morbid_obesity +
+#                           chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
+#                           chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
+#                           stroke + asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
+#                           immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
+#                           shielded_since_feb_15 + (1 | practice_id),
+#                         data = data_cox)
+# 
+# write_rds(mod.coxme.adj, here::here("output", "models", "mod_coxme_adj.rds"), compress="gz")
 
 
 # Output model coefficients ----
@@ -76,9 +77,9 @@ gtsave(tab_mod1 %>% as_gt(), here::here("output", "models", "tab_coxph.html"))
 write_csv(tab_mod1$table_body, here::here("output",  "models", "tab_coxph.csv"))
 
 ### Mixed effects Cox model - adjusted
-tab_mod2 <- gtsummary::tbl_regression(mod.coxme.adj)
-gtsave(tab_mod2 %>% as_gt(), here::here("output", "models", "tab_coxme.html"))
-write_csv(tab_mod2$table_body, here::here("output",  "models", "tab_coxme.csv"))
+# tab_mod2 <- gtsummary::tbl_regression(mod.coxme.adj)
+# gtsave(tab_mod2 %>% as_gt(), here::here("output", "models", "tab_coxme.html"))
+# write_csv(tab_mod2$table_body, here::here("output",  "models", "tab_coxme.csv"))
 
 ## Forest plots
 
@@ -91,9 +92,9 @@ ggsave(
 )
 
 ### Mixed effects Cox model - adjusted
-plot_coxme <- survminer::ggforest(mod.coxme.adj, data = data_cox)
-ggsave(
-  here::here("output", "plot_coxph.svg"),
-  plot_coxph,
-  units = "cm", width = 20, height = 25
-)
+# plot_coxme <- survminer::ggforest(mod.coxme.adj, data = data_cox)
+# ggsave(
+#   here::here("output", "plot_coxph.svg"),
+#   plot_coxph,
+#   units = "cm", width = 20, height = 25
+# )
