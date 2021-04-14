093400c7dc0d74f978f4c92fb2aa7deb3900b9f1
acagreen17 <acagreen17@gmail.com>
2021-04-14 16:03:30 +0100

    Practice id as stratification variable

--- analysis/R/Scripts/02_Models.R
+++ analysis/R/Scripts/02_Models.R
@@ -61,7 +61,7 @@ mod.coxph.adjb <- coxph(Surv(follow_up_time, covid_vax) ~
                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
                          immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
-                         shielded_since_feb_15 + as.factor(practice_id),
+                         shielded_since_feb_15 + strata(practice_id),
                        data = data_cox)
 
 write_rds(mod.coxph.adjb, here::here("output", "models", "mod_coxph_adjb.rds"), compress="gz")
