f4ea033e19e168c4c3a5dc8215221c7d193b4c63
acagreen17 <acagreen17@gmail.com>
2021-04-20 09:26:08 +0100

    add

--- analysis/R/Scripts/02_Models_sub_test.R
+++ analysis/R/Scripts/02_Models_sub_test.R
@@ -51,7 +51,7 @@ print(length(unique(data_cox_stratification$practice_id)))
 
 # Mixed effects Cox model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & pracice as random effect
 mod.coxme.adj <- coxme(Surv(follow_up_time, covid_vax) ~
-                         ageband + + sex + morbid_obesity +
+                         ageband + + sex + ethnicity + morbid_obesity +
                          chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
