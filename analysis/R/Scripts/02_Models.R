6811be9e480a883bfd5056da535ad0a26759ddda
acagreen17 <acagreen17@gmail.com>
2021-04-20 16:08:16 +0100

    Model comparisons

--- analysis/R/Scripts/02_Models.R
+++ analysis/R/Scripts/02_Models.R
@@ -49,56 +49,56 @@ data_cox_stratification <- data_cox %>%
 
 # MODELS ----
 
-# ## Cox PH model - unadjusted
-# mod.coxph.unadj <- coxph(Surv(follow_up_time, covid_vax) ~ 1, data = data_cox)
-# 
-# write_rds(mod.coxph.unadj, here::here("output", "models", "testing", "mod_coxph_unadj.rds"), compress="gz")
-# 
-# ## Cox PH model - adjusted; baseline demographics, comorbs, geographical, flu and shielding 
-# mod.coxph.adj <- coxph(Surv(follow_up_time, covid_vax) ~ 
-#                          ageband + sex + ethnicity + morbid_obesity +
-#                          chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
-#                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
-#                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
-#                          immunosuppression_medication + imd + stp + region + rural_urban + flu_vaccine + shielded +
-#                          shielded_since_feb_15,
-#                        data = data_cox)
-# 
-# write_rds(mod.coxph.adj, here::here("output", "models", "testing", "mod_coxph_adj.rds"), compress="gz")
-# 
-# # Cox model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & practice id
-# mod.coxph.adjb <- coxph(Surv(follow_up_time, covid_vax) ~
-#                          ageband + sex + ethnicity + morbid_obesity +
-#                          chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
-#                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
-#                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
-#                          immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
-#                          shielded_since_feb_15 + strata(practice_id),
-#                        data = data_cox_stratification)
-# 
-# write_rds(mod.coxph.adjb, here::here("output", "models", "testing", "mod_coxph_adjb.rds"), compress="gz")
-# 
-# # Cox model with RE for practice - adjusted; baseline demographics, comorbs, geographical, flu, shielding & practice id
-# mod.coxph.adjc <- coxph(Surv(follow_up_time, covid_vax) ~
-#                           ageband + sex + ethnicity + morbid_obesity +
-#                           chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
-#                           chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
-#                           asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
-#                           immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
-#                           shielded_since_feb_15 + frailty(practice_id),
-#                         data = data_cox_stratification)
-# 
-# write_rds(mod.coxph.adjc, here::here("output", "models", "testing", "mod_coxph_adjc.rds"), compress="gz")
+## Cox PH model - unadjusted
+mod.coxph.unadj <- coxph(Surv(follow_up_time, covid_vax) ~ 1, data = data_cox)
 
+write_rds(mod.coxph.unadj, here::here("output", "models", "testing", "mod_coxph_unadj.rds"), compress="gz")
 
-# Mixed effects Cox model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & pracice as random effect
-mod.coxme.adj <- coxme(Surv(follow_up_time, covid_vax) ~
-                         ageband + sex + morbid_obesity +
+## Cox PH model - adjusted; baseline demographics, comorbs, geographical, flu and shielding
+mod.coxph.adj <- coxph(Surv(follow_up_time, covid_vax) ~
+                         ageband + sex + ethnicity + morbid_obesity +
+                         chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
+                         chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
+                         asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
+                         immunosuppression_medication + imd + stp + region + rural_urban + flu_vaccine + shielded +
+                         shielded_since_feb_15,
+                       data = data_cox)
+
+write_rds(mod.coxph.adj, here::here("output", "models", "testing", "mod_coxph_adj.rds"), compress="gz")
+
+# Cox model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & practice id
+mod.coxph.adjb <- coxph(Surv(follow_up_time, covid_vax) ~
+                         ageband + sex + ethnicity + morbid_obesity +
                          chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
                          immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
-                         shielded_since_feb_15 + (1 | practice_id),
+                         shielded_since_feb_15 + strata(practice_id),
                        data = data_cox_stratification)
 
-write_rds(mod.coxme.adj, here::here("output", "models", "testing", "mod_coxme_adj.rds"), compress="gz")
+write_rds(mod.coxph.adjb, here::here("output", "models", "testing", "mod_coxph_adjb.rds"), compress="gz")
+
+# Cox model with RE for practice - adjusted; baseline demographics, comorbs, geographical, flu, shielding & practice id
+mod.coxph.adjc <- coxph(Surv(follow_up_time, covid_vax) ~
+                          ageband + sex + ethnicity + morbid_obesity +
+                          chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
+                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
+                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
+                          immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
+                          shielded_since_feb_15 + factor(practice_id),
+                        data = data_cox_stratification)
+
+write_rds(mod.coxph.adjc, here::here("output", "models", "testing", "mod_coxph_adjc.rds"), compress="gz")
+
+
+# Mixed effects Cox model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & pracice as random effect
+# mod.coxme.adj <- coxme(Surv(follow_up_time, covid_vax) ~
+#                          ageband + sex + morbid_obesity +
+#                          chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
+#                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
+#                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
+#                          immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
+#                          shielded_since_feb_15 + (1 | practice_id),
+#                        data = data_cox_stratification)
+# 
+# write_rds(mod.coxme.adj, here::here("output", "models", "testing", "mod_coxme_adj.rds"), compress="gz")
