14dc265f7b60415fef36d7af3bf4f897c9d1b94a
acagreen17 <acagreen17@gmail.com>
2021-04-21 13:29:30 +0100

    Models

--- analysis/R/Scripts/02_Models.R
+++ analysis/R/Scripts/02_Models.R
@@ -2,9 +2,8 @@
 
 # This script:
 # - imports processed data
+# - fits several cox models using the coxph package
 # - fits a mixed effects cox model using the coxme package
-# - saves model summaries (tables and figures)
-
 ######################################
 
 
@@ -23,7 +22,7 @@ library('gt')
 dir.create(here::here("output", "models", "testing"), showWarnings = FALSE, recursive=TRUE)
 
 ## Import processed data
-data_tte <- read_rds(here::here("output", "data", "data_all.rds"))
+data_tte <- read_rds(here::here("output", "data", "data_modelling.rds"))
 
 ## Converts logical to integer so that model coefficients print nicely in gtsummary methods
 data_cox <- data_tte %>%
@@ -38,12 +37,12 @@ data_cox <- data_tte %>%
 # 
 # ### Registered patients counts
 # practice_counts <- data_cox %>% 
-#   group_by(practice_id) %>%
+#   group_by(practice_id_latest_active_registration) %>%
 #   summarise(`Number_of_registered_patients` = n())
 # 
 # ## Exclude 
 # data_cox_stratification <- data_cox %>%
-#   filter(practice_id %in% subset(practice_counts, Number_of_registered_patients >= 100)$practice_id)
+#   filter(practice_id_latest_active_registration %in% subset(practice_counts, Number_of_registered_patients >= 100)$practice_id_latest_active_registration)
 
 
 
@@ -73,11 +72,22 @@ mod.coxph.adjb <- coxph(Surv(follow_up_time, covid_vax) ~
                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
                          immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
-                         shielded_since_feb_15 + strata(practice_id),
+                         shielded_since_feb_15 + practice_id_latest_active_registration,
                        data = data_cox)
 
 write_rds(mod.coxph.adjb, here::here("output", "models", "testing", "mod_coxph_adjb.rds"), compress="gz")
 
+# Cox model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & practice id
+mod.coxph.adjc <- coxph(Surv(follow_up_time, covid_vax) ~
+                          ageband + sex + ethnicity + morbid_obesity +
+                          chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
+                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
+                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
+                          immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
+                          shielded_since_feb_15 + strata(practice_id_latest_active_registration),
+                        data = data_cox)
+
+write_rds(mod.coxph.adjc, here::here("output", "models", "testing", "mod_coxph_adjb.rds"), compress="gz")
 
 # Mixed effects Cox model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & pracice as random effect
 # mod.coxme.adj <- coxme(Surv(follow_up_time, covid_vax) ~
@@ -86,7 +96,7 @@ write_rds(mod.coxph.adjb, here::here("output", "models", "testing", "mod_coxph_a
 #                          chronic_kidney_disease_all_stages_1_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
 #                          asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
 #                          immunosuppression_medication + imd + region + rural_urban + flu_vaccine + shielded +
-#                          shielded_since_feb_15 + (1 | practice_id),
+#                          shielded_since_feb_15 + (1 | practice_id_latest_active_registration),
 #                        data = data_cox_stratification)
 # 
 # write_rds(mod.coxme.adj, here::here("output", "models", "testing", "mod_coxme_adj.rds"), compress="gz")
