c65dd449a72f838322ce4f4546ff96edc4a7c5c0
acagreen17 <acagreen17@gmail.com>
2021-05-04 20:24:19 +0100

    rename

---
+++ analysis/R/Scripts/02b_models_coxme.R
@@ -0,0 +1,67 @@
+######################################
+
+# This script:
+# - imports processed data
+# - fits a mixed effects cox model using the coxme package to a subset of the data
+# - saves model summaries (tables and figures)
+
+######################################
+
+
+# Preliminaries ----
+
+## Import libraries
+library('tidyverse')
+library('lubridate')
+library('survival')
+library('coxme')
+library('gtsummary')
+library('gt')
+#library('ehahelper')
+
+## Create output directory
+dir.create(here::here("output", "models", "testing"), showWarnings = FALSE, recursive=TRUE)
+
+## Import processed data
+data_tte <- read_rds(here::here("output", "data", "data_modelling.rds"))
+
+## Converts logical to integer so that model coefficients print nicely in gtsummary methods
+data_cox <- data_tte %>%
+  mutate(
+    across(
+      where(is.logical),
+      ~.x*1L
+    )
+  )
+
+
+# Subset data
+
+## Registered patients counts
+practice_counts <- data_cox %>% 
+  group_by(practice_id_latest_active_registration) %>%
+  summarise(`Number_of_registered_patients` = n())
+
+## Exclude 
+# data_cox_reduced <- data_cox %>%
+#   filter(#practice_id_latest_active_registration %in% 
+#          #  subset(practice_counts, Number_of_registered_patients >= 100)$practice_id_latest_active_registration,
+#          practice_id_latest_active_registration %in% 
+#            subset(practice_counts, Number_of_registered_patients >= 100)$practice_id_latest_active_registration[1:1000]) %>%
+#   droplevels()
+
+
+
+# MODELS ----
+
+# Mixed effects Cox model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & pracice as random effect
+mod.coxme.adj <- coxme(Surv(follow_up_time, covid_vax) ~
+                         ageband + sex + ethnicity + morbid_obesity +
+                         chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
+                         chronic_kidney_disease_all_stages_3_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
+                         asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
+                         immunosuppression_medication + imd + rural_urban + prior_covid + flu_vaccine + shielded + shielded_since_feb_15 + 
+                         rural_urban + region + (1 | practice_id_latest_active_registration),
+                       data = data_cox)
+
+write_rds(mod.coxme.adj, here::here("output", "models", "testing", "mod_test_coxme_adj.rds"), compress="gz")
---
+++ analysis/R/Scripts/02d_models_cox_vs_aft.R
@@ -0,0 +1,64 @@
+######################################
+
+# This script:
+# - imports processed data
+# - fits a number of stratified coxph and AFT models to several subsets of the data
+
+######################################
+
+
+# Preliminaries ----
+
+## Import libraries
+library('tidyverse')
+library('lubridate')
+library('survival')
+library('gtsummary')
+library('gt')
+
+## Create output directory
+dir.create(here::here("output", "models", "cox_vs_aft"), showWarnings = FALSE, recursive=TRUE)
+
+## Import processed data
+data_tte <- read_rds(here::here("output", "data", "data_modelling.rds"))
+
+## Converts logical to integer so that model coefficients print nicely in gtsummary methods
+data_cox <- data_tte %>%
+  mutate(
+    across(
+      where(is.logical),
+      ~.x*1L
+    )
+  )
+
+
+# MODELS ----
+
+## Stratified Cox model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & practice as strata
+mod.stratcoxph.adj <- coxph(Surv(follow_up_time, covid_vax) ~
+                              ageband + sex + ethnicity + morbid_obesity +
+                              chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
+                              chronic_kidney_disease_all_stages_3_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
+                              asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
+                              immunosuppression_medication + imd + rural_urban + prior_covid + flu_vaccine + shielded + shielded_since_feb_15 + 
+                              rural_urban + region + strata(practice_id_latest_active_registration),
+                            data = data_cox)
+
+write_rds(mod.stratcoxph.adj, here::here("output", "models", "cox_vs_aft", "mod_stratcoxph_adj.rds"), compress="gz")
+
+# ## AFT model - adjusted; baseline demographics, comorbs, geographical, flu, shielding & practice as random effect
+# mod.aft.re.adj <- survreg(Surv(follow_up_time, covid_vax) ~
+#                             ageband + sex + ethnicity + morbid_obesity +
+#                             chronic_heart_disease + diabetes + chronic_kidney_disease_diagnostic + chronic_kidney_disease_all_stages +
+#                             chronic_kidney_disease_all_stages_3_5 + sev_mental_ill + learning_disability + chronic_neuro_dis_inc_sig_learn_dis +
+#                             asplenia + chronic_liver_disease + chronis_respiratory_disease + immunosuppression_diagnosis +
+#                             immunosuppression_medication + imd + rural_urban + prior_covid + flu_vaccine + shielded + shielded_since_feb_15 + 
+#                             rural_urban + region + frailty(practice_id_latest_active_registration),
+#                           data = data_cox)
+# 
+# write_rds(mod.aft.re.adj, here::here("output", "models", "cox_vs_aft", "mod_aft_re_adj.rds"), compress="gz")
+# 
+
+
+
+  
