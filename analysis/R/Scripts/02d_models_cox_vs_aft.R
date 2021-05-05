f6dbc403143c052e1b1220e5445592bc46f20739
acagreen17 <acagreen17@gmail.com>
2021-05-05 10:52:14 +0100

    remove rank

--- analysis/R/Scripts/02d_models_cox_vs_aft.R
+++ analysis/R/Scripts/02d_models_cox_vs_aft.R
@@ -47,7 +47,7 @@ mod.stratcoxph.adj <- coxph(Surv(follow_up_time, covid_vax) ~
 write_rds(mod.stratcoxph.adj, here::here("output", "models", "cox_vs_aft", "mod_stratcoxph_adj.rds"), compress="gz")
 
 cox.zph(mod.stratcoxph.adj, transform = "identity")$table
-cox.zph(mod.stratcoxph.adj, transform = "rank")$table
+#cox.zph(mod.stratcoxph.adj, transform = "rank")$table
 cox.zph(mod.stratcoxph.adj, transform = "log") $table
 cox.zph(mod.stratcoxph.adj, transform = "km")$table
 
