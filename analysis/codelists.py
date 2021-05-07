997e75d037b7353aa9782bb01bb4938f3eec2055
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-05-07 10:19:33 +0100

    update probable covid codes to draft snomed codes

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -207,19 +207,19 @@ covid_codes = codelist_from_csv(
 )
 
 covid_primary_care_code = codelist_from_csv(
-  "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
+  "codelists/user-will-hulme-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
   system = "ctv3",
   column = "CTV3ID",
 )
 
 covid_primary_care_positive_test = codelist_from_csv(
-  "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
+  "codelists/user-will-hulme-covid-identification-in-primary-care-probable-covid-positive-test.csv",
   system = "ctv3",
   column = "CTV3ID",
 )
 
 covid_primary_care_sequalae = codelist_from_csv(
-  "codelists/opensafely-covid-identification-in-primary-care-probable-covid-sequelae.csv",
+  "codelists/user-will-hulme-covid-identification-in-primary-care-probable-covid-sequelae.csv",
   system = "ctv3",
   column = "CTV3ID",
 )
