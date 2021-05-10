9be1de7acd284eb5afca537f110cd95dad89a761
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-05-10 16:16:09 +0100

    add deciles on one plot

--- analysis/R/Scripts/04_strata_summary.R
+++ analysis/R/Scripts/04_strata_summary.R
@@ -329,3 +329,33 @@ ggsave(
   CHdeciles,
   units = "cm", width = 20, height = 20
 )
+
+
+## combined plot
+
+
+
+plot_strata_combined <- ggplot()+
+  geom_step(data = strata_estimates, aes(x=as.Date("2020-12-08")+time, y=cml.haz, group=strata), alpha=0.1)+
+  geom_step(data = strata_quantiles %>% filter(cml.haz_q %in% seq(0.1,0.9,0.1)), aes(x=as.Date("2020-12-08")+time, y=cml.haz, group=cml.haz_q), alpha=0.9, colour='darkred', size=1)+
+  scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"), limits = c(as.Date("2020-12-01"), "2021-03-17"))+
+  labs(
+    x="Date", y="Cumulative hazard"
+  )+
+  theme_bw()+
+  theme(
+    panel.border = element_blank(),
+    axis.line.x = element_line(colour = "black"),
+    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+
+
+ggsave(
+  here::here("output", "models", "final", "plot_strata_combined.svg"),
+  plot_strata_combined,
+  units = "cm", width = 20, height = 20
+)
+
+
