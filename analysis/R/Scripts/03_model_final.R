2d5e98e7dcf676e78b8d9553341d233fed161cac
acagreen17 <acagreen17@gmail.com>
2021-05-11 17:11:05 +0100

    format correctly (!)

--- analysis/R/Scripts/03_model_final.R
+++ analysis/R/Scripts/03_model_final.R
@@ -14,7 +14,6 @@
 library('tidyverse')
 library('lubridate')
 library('survival')
-library('coxme')
 library('gtsummary')
 library('gt')
 library('survminer')
@@ -31,11 +30,11 @@ data_tte <- read_rds(here::here("output", "data", "data_modelling.rds"))
 
 ## Stratified Cox PH model - adjusted; baseline demographics + comorbs, 
 mod.strat.coxph.adj <- coxph(Surv(follow_up_time, covid_vax) ~
-                               ageband, sex, ethnicity, imd, immunosuppression, ckd, 
-                             chronic_respiratory_disease, diabetes, chronic_liver_disease, 
-                             chronic_neuro_dis_inc_sig_learn_dis, chronic_heart_disease, asplenia, 
-                             sev_mental_ill, morbid_obesity + strata(practice_id_latest_active_registration),
-                             data = data_cox)
+                               ageband + sex + ethnicity + imd + immunosuppression + ckd + 
+                             chronic_respiratory_disease + diabetes + chronic_liver_disease + 
+                             chronic_neuro_dis_inc_sig_learn_dis + chronic_heart_disease + asplenia + 
+                             sev_mental_ill + morbid_obesity + strata(practice_id_latest_active_registration),
+                             data = data_tte)
 
 write_rds(mod.strat.coxph.adj, here::here("output", "models", "final", "mod_strat_coxph_adj.rds"), compress="gz")
 
