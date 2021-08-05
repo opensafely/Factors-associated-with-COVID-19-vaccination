48e3a2c1d01ba511c9b5e1d1c7f6860509a6bcf8
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-08-05 14:44:40 +0100

    Add scripts for meta analysis
    This commit creates a csv that combines model estimates from tpp and emis backends.
    
    The first script should be run inside each backend (and has a corresponding action in project.yaml). This takes the previously created model objects and saves complete model summary information (using tidy_plus_plus).
    
    The second script should only be run offline. It combines the outputted model coefficients from each backend using inverse-variance-weighting

---
+++ analysis/R/Scripts/98_moveoutputs.R
@@ -0,0 +1,50 @@
+######################################
+
+# This script copies EMIS/TPP outputs needed for meta analysis
+# Models are imported, model summaries are created in a standard format, and then saved in a "combined" folder
+# (could change to just copy existing outputs if they were suitably formatted)
+
+######################################
+
+
+
+## Import libraries ----
+library('here')
+library('glue')
+library('fs')
+
+## create output directory ----
+dir_create(here("output", "combined"))
+
+## custom functions ----
+tidy_wald <- function(x, conf.int = TRUE, conf.level = .95, exponentiate = TRUE, ...) {
+  
+  # to use Wald CIs instead of profile CIs.
+  ret <- broom::tidy(x, conf.int = FALSE, conf.level = conf.level, exponentiate = exponentiate)
+  
+  if(conf.int){
+    ci <- confint.default(x, level = conf.level)
+    if(exponentiate){ci = exp(ci)}
+    ci <- as_tibble(ci, rownames = "term")
+    names(ci) <- c("term", "conf.low", "conf.high")
+    
+    ret <- dplyr::left_join(ret, ci, by = "term")
+  }
+  ret
+}
+
+# import data and model, and create tidy summary ----
+# need to import data too otherwise tidy functions don't work
+
+data_tte <- read_rds(here("output", "data", "data_modelling.rds"))
+model <- read_rds(here("output", "models", "final", "mod_strat_coxph_adj.rds"))
+tidy_model <- broom.helpers::tidy_plus_plus(model, tidy_fun=tidy_wald, exponentiate=FALSE)
+
+if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
+  for(backend in c("tpp", "emis")){
+    write_csv(tidy_model, here("output", "combined", glue("tidy_{backend}.csv")))
+  }
+} else {
+  write_csv(tidy_model, here("output", "combined", glue("tidy_{Sys.getenv('OPENSAFELY_BACKEND')}.csv")))
+}
+
