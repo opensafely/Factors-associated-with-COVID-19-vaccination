55134bcaba140444f35a248ef4dadc852f620585
acagreen17 <acagreen17@gmail.com>
2021-08-10 16:45:12 +0100

    revert back to single action

--- analysis/R/Scripts/03_model_final.R
+++ analysis/R/Scripts/03_model_final.R
@@ -63,16 +63,16 @@ mod.strat.coxph.adj <- coxph(Surv(follow_up_time, covid_vax) ~
 ## Save model
 write_rds(mod.strat.coxph.adj, here::here("output", "model", "mod_strat_coxph_adj.rds"), compress="gz")
 
-# ## Save a "tidy" copy of each model output. Create "dummy" emis/tpp outputs (identical) for use with combine script
-# #tidy_model <- broom.helpers::tidy_plus_plus(mod.strat.coxph.adj, tidy_fun = tidy_wald, exponentiate = FALSE)
-# tidy_model <- tidy_wald(mod.strat.coxph.adj, conf.int = TRUE, conf.level = .95, exponentiate = TRUE)
-#   
-# 
-# if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
-#   for(backend in c("tpp", "emis")){
-#     write_csv(tidy_model, here("output", "model", glue("tidy_{backend}.csv")))
-#   }
-# } else {
-#   write_csv(tidy_model, here("output", "model", glue("tidy_{Sys.getenv('OPENSAFELY_BACKEND')}.csv")))
-# }
+## Save a "tidy" copy of each model output. Create "dummy" emis/tpp outputs (identical) for use with combine script
+#tidy_model <- broom.helpers::tidy_plus_plus(mod.strat.coxph.adj, tidy_fun = tidy_wald, exponentiate = FALSE)
+tidy_model <- tidy_wald(mod.strat.coxph.adj, conf.int = TRUE, conf.level = .95, exponentiate = TRUE)
+
+
+if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
+  for(backend in c("tpp", "emis")){
+    write_csv(tidy_model, here("output", "model", glue("tidy_{backend}.csv")))
+  }
+} else {
+  write_csv(tidy_model, here("output", "model", glue("tidy_{Sys.getenv('OPENSAFELY_BACKEND')}.csv")))
+}
 
