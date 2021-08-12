0a63b988375e3ae8ee398364b2d656e97d7a6ce4
acagreen17 <acagreen17@gmail.com>
2021-08-12 10:55:15 +0100

    don't exponentiate estimates (combine first)

--- analysis/R/Scripts/03_model_final.R
+++ analysis/R/Scripts/03_model_final.R
@@ -64,8 +64,7 @@ mod.strat.coxph.adj <- coxph(Surv(follow_up_time, covid_vax) ~
 write_rds(mod.strat.coxph.adj, here::here("output", "model", "mod_strat_coxph_adj.rds"), compress="gz")
 
 ## Save a "tidy" copy of each model output. Create "dummy" emis/tpp outputs (identical) for use with combine script
-tidy_model <- broom.helpers::tidy_plus_plus(mod.strat.coxph.adj, tidy_fun = tidy_wald, exponentiate = TRUE)
-
+tidy_model <- broom.helpers::tidy_plus_plus(mod.strat.coxph.adj, tidy_fun = tidy_wald, exponentiate = FALSE)
 
 if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
   for(backend in c("tpp", "emis")){
